76e2f66eff23d63479cd334ea93f497f03e8837d
Prashant jogi <jogiprashant@gmail.com>
2022-02-20 14:30:29 +0530

    added accordian

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('hello!')
+(function registerListenerToPanel(){
+    const list= document.getElementsByClassName("acc-list");
+    if(list){
+        for (let index = 0; index < list.length; index++) {
+            const element = list[index];
+            element.addEventListener("click",function(){              
+               const panel= this.querySelector(".panel-content");
+               const panelHeight=panel.style.height=="0px" || panel.style.height=='';
+               panel.style.height=panelHeight?"80px":"0px";              
+            })            
+        }
+    }
+})();
+
